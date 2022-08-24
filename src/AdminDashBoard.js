a5adcd39d3e24ea6ce7d8471a5d0128bdf24f43c
Abhay Sharma <abhaysharmajr@gmail.com>
2022-08-24 10:35:04 +0530

    AdminPage Dashboard

--- src/AdminDashBoard.js
+++ src/AdminDashBoard.js
@@ -1,17 +1,53 @@
 import "./AdminDashboard.css";
 import { AdminAppBar } from "./AdminAppBar";
+import { useEffect, useState } from "react";
+import { API } from "./global";
+import { useNavigate, useParams } from "react-router-dom";
+
 export function AdminDashboard() {
   
+  function GetMovies(){
+    const res=fetch(`${API}/movies`,{
+      method:"GET",
+          headers:{
+            "content-Type":"application/json",
+            "x-auth-token":localStorage.getItem('token'),
+          }
+    });
+    res.then((data)=>data.json())
+    .then((mvs)=>setMovieList(mvs));
+}
+
+useEffect(()=>{
+  GetMovies()
+  },[])
+  
+const[MovieList,setMovieList]=useState([]);
   return (
     <div className="AdminDashboard-container">
-<AdminAppBar/>
+      <AdminAppBar/>
+    
+   
 
 <div className="AdminDashboard-MovieDetails">
+        
 
-</div>
-<h1>Admin DashBoard</h1>
+{MovieList.map((movie)=>(
+        <DisplayAllMoviesAdmin movie={movie} key={movie._id} movieid={movie._id}/>
+      ))}
     </div>
+    </div>
+   
   );
 }
 
 
+function DisplayAllMoviesAdmin({movie,movieid}){
+  const navigate=useNavigate();
+  const{id}=useParams();
+  return(
+    <>
+        <img src={movie.poster} alt={movie.name} className="All-movies-AdminPage" onClick={()=>navigate(`/movie/${id}/${movieid}`)} />
+    </>
+  )
+}
