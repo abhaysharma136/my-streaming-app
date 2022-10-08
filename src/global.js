d8d5ae338fa1afb4c3485ca0c91fcded244509ff
Abhay Sharma <abhaysharmajr@gmail.com>
2022-10-08 18:59:10 +0530

    Global

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-// export const API = `https://onstream-node-app.herokuapp.com`;
-export const API=`http://localhost:5000`;
+export const API = `https://onstream-node-app.herokuapp.com`;
+// export const API=`http://localhost:5000`;
