8f2617b87d5c581bd9d14c17b1e104e5b001fafd
Abhay Sharma <abhaysharmajr@gmail.com>
2025-09-10 11:50:04 +0530

    code fixed for vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
-import netlify from "@netlify/vite-plugin";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), netlify()],
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Explicitly set output directory
+  },
 });
